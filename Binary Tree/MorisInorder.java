21c85c6b4059b18ca0af099197c9994288675cd9
DhaanishNihaal <kit27.am13@gmail.com>
2025-12-17 10:13:31 +0530

    morisinorder

---
+++ Binary Tree/MorisInorder.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        TreeNode cur=root;
+        ArrayList<Integer> arr=new ArrayList<>();
+        while(cur!=null){
+            if(cur.left==null){
+                arr.add(cur.val);
+                cur=cur.right;
+            }
+            else{
+                TreeNode prev=cur.left;
+                while(prev.right!=null && prev.right!=cur){
+                    prev=prev.right;
+                }
+                if(prev.right==null){
+                    prev.right=cur;
+                    cur=cur.left;
+                }
+                else{
+                    arr.add(cur.val);
+                    prev.right=null;
+                    cur=cur.right;
+                }
+            }
+        }
+        return arr;
+    }
+}
