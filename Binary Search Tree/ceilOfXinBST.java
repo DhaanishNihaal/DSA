730ae0eb4860f44b503be07ce9b6ca30cedf8763
DhaanishNihaal <kit27.am13@gmail.com>
2025-12-17 11:16:12 +0530

    ceil

---
+++ Binary Search Tree/ceilOfXinBST.java
@@ -0,0 +1,16 @@
+class Solution {
+    int findCeil(Node root, int x) {
+        // code here
+        int ceil=-1;
+        while(root!=null){
+            if(root.data>=x){
+                ceil=root.data;
+                root=root.left;
+            }
+            else{
+                root=root.right;
+            }
+        }
+        return ceil;
+    }
+}
