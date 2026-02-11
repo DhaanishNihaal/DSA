3b49723c2c747c8d8c2660fb42dfebed2db009e2
DhaanishNihaal <kit27.am13@gmail.com>
2026-02-11 16:51:56 +0530

    bellman ford

---
+++ Graphs/BellmanFord.java
@@ -0,0 +1,24 @@
+// User function Template for Java
+
+class Solution {
+    public int[] bellmanFord(int V, int[][] edges, int src) {
+        // code here
+        int[] dist=new int[V];
+        Arrays.fill(dist,(int)1e8);
+        dist[src]=0;
+        for(int i=0;i<V-1;i++){
+            for(int[] edge:edges){
+                if(dist[edge[0]]!=(int)1e8 && dist[edge[1]]>dist[edge[0]]+edge[2]){
+                    dist[edge[1]]=dist[edge[0]]+edge[2];
+                }
+            }
+        }
+        for(int[] edge:edges){
+            if(dist[edge[0]]!=(int)1e8 && dist[edge[1]]>dist[edge[0]]+edge[2]){
+                    return new int[]{-1};
+                }
+        }
+        return dist;
+        
+    }
+}
